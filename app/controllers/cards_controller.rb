09e32f5222a7481fc27fa076ecfa0961167f8229
simplesasha <breezymsk@gmail.com>
2016-09-13 10:55:29 +0300

    Добавлено создание/редактирование/удаление карточек

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,37 @@
 class CardsController < ApplicationController
   def index
-    @card = Card.all.order(:id)
+    @card = Card.all
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.create(params[:card].permit(:original_text, :translated_text, :review_date))
+  	if @card.save
+  	  redirect_to cards_path
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to cards_path
+  end  
+
+  def edit
+    @card = Card.find(params[:id]) 
+  end 
+
+  def update
+  	@card = Card.find(params[:id])
+  	if @card.update(params[:card].permit(:original_text, :translated_text, :review_date))
+  	  redirect_to cards_path
+  	else
+  	  render 'edit'
+  	end
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,20 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validates :review_date, presence: true
+  validate :same_fields
+
+  before_validation :update_review_date, on: :create
+
+  protected
+
+  def update_review_date
+    self.review_date = 3.days.since
+  end
+
+  def same_fields
+    if original_text.downcase == translated_text.downcase
+      errors.add(:translated_text, "Оригинал и перевод не могут быть одинаковыми!")
+    end
+  end  
 end
